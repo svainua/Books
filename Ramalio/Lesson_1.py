d31864e8aab268cda888543ac56ebbb72f65ca67
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-03-04 22:48:23 +0100

    uod

--- Ramalio/Lesson_1.py
+++ Ramalio/Lesson_1.py
@@ -3,30 +3,28 @@ import random
 
 Card = collections.namedtuple("Card", ["rank", "suit"])
 
+
 class FrenchDeck:
-    ranks = [str(n) for n in range(2,11)] + list("JQKA")
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
     suits = "spades diamonds clubs hearts".split()
 
     def __init__(self) -> None:
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self._cards = [
+            Card(rank, suit) for suit in self.suits for rank in self.ranks
+        ]
 
     def __len__(self):
         return len(self._cards)
-    
+
     def __getitem__(self, position):
         return self._cards[position]
-    
+
 
 beer_card = Card("7", "diamonds")
-#print(beer_card)
+# print(beer_card)
 
 deck = FrenchDeck()
 print(len(deck))
 
 random_card = random.choice(deck)
 print(random_card)
-
-
-
-
-
